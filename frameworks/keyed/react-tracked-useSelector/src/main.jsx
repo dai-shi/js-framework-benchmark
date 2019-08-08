d6e3684c30d73862053ada2452f5f58a7bf26427
daishi <daishi@axlight.com>
2019-08-08 19:31:08 +0900

    renew reactive-react-redux and react-tracked from react-redux-hooks

--- frameworks/keyed/react-tracked-useSelector/src/main.jsx
+++ frameworks/keyed/react-tracked-useSelector/src/main.jsx
@@ -1,17 +1,17 @@
-import React, { useCallback, useReducer } from 'react';
-import ReactDOM from 'react-dom';
-import { Provider, useSelector, useDispatch } from 'react-tracked';
+import React, { useCallback, useReducer } from "react";
+import ReactDOM from "react-dom";
+import { Provider, useDispatch, useSelector } from "react-tracked";
 
-function random(max) {
-  return Math.round(Math.random() * 1000) % max;
-}
-
-const A = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
+const A = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean",
+  "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive",
+  "cheap", "expensive", "fancy"];
 const C = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
-const N = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
+const N = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse",
+  "keyboard"];
+
+const random = (max) => Math.round(Math.random() * 1000) % max;
 
 let nextId = 1;
-
 function buildData(count) {
   const data = new Array(count);
   for (let i = 0; i < count; i++) {
@@ -27,120 +27,90 @@ const initialState = { data: [], selected: 0 };
 const reducer = (state, action) => {
   const { data, selected } = state;
   switch (action.type) {
-    case 'RUN':
+    case "RUN":
       return { data: buildData(1000), selected: 0 };
-    case 'RUN_LOTS':
+    case "RUN_LOTS":
       return { data: buildData(10000), selected: 0 };
-    case 'ADD':
+    case "ADD":
       return { data: data.concat(buildData(1000)), selected };
-    case 'UPDATE':
+    case "UPDATE": {
       const newData = data.slice();
       for (let i = 0; i < newData.length; i += 10) {
         const r = newData[i];
         newData[i] = { id: r.id, label: r.label + " !!!" };
       }
       return { data: newData, selected };
-    case 'REMOVE':
-      const idx = data.findIndex((d) => d.id === action.id);
-      return { data: [...data.slice(0, idx), ...data.slice(idx + 1)], selected };
-    case 'SELECT':
+    }
+    case "REMOVE": {
+      const newData = data.slice();
+      newData.splice(data.indexOf(action.item), 1);
+      return { data: newData, selected };
+    }
+    case "SELECT":
       return { data, selected: action.id };
-    case 'CLEAR':
+    case "CLEAR":
       return { data: [], selected: 0 };
-    case 'SWAP_ROWS':
-      return { data: [data[0], data[998], ...data.slice(2, 998), data[1], data[999]], selected };
+    case "SWAP_ROWS": {
+      const newData = data.slice();
+      const tmp = newData[1];
+      newData[1] = newData[998];
+      newData[998] = tmp;
+      return { data: newData, selected };
+    }
   }
   return state;
 };
 const useValue = () => useReducer(reducer, initialState);
 
-function select(id) {
-  return { type: 'SELECT', id };
-}
-
-function remove(id) {
-  return { type: 'REMOVE', id };
-}
-
 const GlyphIcon = <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>;
 
-const Row = React.memo(({ i }) => {
-  const selector = useCallback((state) => {
-    const item = state.data[i];
-    return state.selected === item.id ? { id: item.id, label: item.label, selected: true } : item;
-  }, [i]);
-  const { selected, id, label } = useSelector(selector);
+const Row = React.memo(({ data }) => {
+  const isSelected = useSelector((state) => state.selected === data.id);
   const dispatch = useDispatch();
-  const onSelect = useCallback(() => {
-    dispatch(select(id));
-  }, [dispatch, id]);
-  const onRemove = useCallback(() => {
-    dispatch(remove(id));
-  }, [dispatch, id]);
+  const select = useCallback(() => { dispatch({ type: "SELECT", id: data.id }); }, [data]);
+  const remove = useCallback(() => { dispatch({ type: "REMOVE", item: data }); }, [data]);
   return (
-    <tr className={selected ? "danger" : ""}>
-      <td className="col-md-1">{id}</td>
-      <td className="col-md-4"><a onClick={onSelect}>{label}</a></td>
-      <td className="col-md-1"><a onClick={onRemove}>{GlyphIcon}</a></td>
+    <tr className={isSelected ? "danger" : ""}>
+      <td className="col-md-1">{data.id}</td>
+      <td className="col-md-4"><a onClick={select}>{data.label}</a></td>
+      <td className="col-md-1"><a onClick={remove}>{GlyphIcon}</a></td>
       <td className="col-md-6"></td>
     </tr>
-  );
+  )
 });
 
-const InnerRowList = React.memo(({ data }) => {
-  return data.map((item, i) => <Row key={item.id} i={i} />);
+const RowList = React.memo(() => {
+  const rows = useSelector((state) => state.data);
+  return rows.map((data) => <Row key={data.id} data={data} />);
 });
 
-const RowList = () => {
-  const data = useSelector(state => state.data);
-  return <InnerRowList data={data} />;
-};
-
-function Button(props) {
-  return (
-    <div className="col-sm-6 smallpad">
-      <button type="button" className="btn btn-primary btn-block" id={props.id} onClick={props.cb}>{props.title}</button>
-    </div>
-  );
-}
+const Button = React.memo(({ id, title, cb }) => (
+  <div className="col-sm-6 smallpad">
+    <button type="button" className="btn btn-primary btn-block" id={id} onClick={cb}>{title}</button>
+  </div>
+));
 
 const Main = () => {
   const dispatch = useDispatch();
-  const run = useCallback(() => {
-    dispatch({ type: 'RUN' });
-  }, [dispatch]);
-  const runLots = useCallback(() => {
-    dispatch({ type: 'RUN_LOTS' });
-  }, [dispatch]);
-  const add = useCallback(() => {
-    dispatch({ type: 'ADD' });
-  }, [dispatch]);
-  const update = useCallback(() => {
-    dispatch({ type: 'UPDATE' });
-  }, [dispatch]);
-  const clear = useCallback(() => {
-    dispatch({ type: 'CLEAR' });
-  }, [dispatch]);
-  const swapRows = useCallback(() => {
-    dispatch({ type: 'SWAP_ROWS' });
-  }, [dispatch]);
+  const run = useCallback(() => { dispatch({ type: "RUN" }); }, []);
+  const runLots = useCallback(() => { dispatch({ type: "RUN_LOTS" }); }, []);
+  const add = useCallback(() => { dispatch({ type: "ADD" }); }, []);
+  const update = useCallback(() => { dispatch({ type: "UPDATE" }); }, []);
+  const clear = useCallback(() => { dispatch({ type: "CLEAR" }); }, []);
+  const swapRows = useCallback(() => { dispatch({ type: "SWAP_ROWS" }); }, []);
   return (
     <div className="container">
       <div className="jumbotron">
         <div className="row">
-          <div className="col-md-6">
-            <h1>react-tracked useSelector</h1>
-          </div>
-          <div className="col-md-6">
-            <div className="row">
-              <Button id="run" title="Create 1,000 rows" cb={run} />
-              <Button id="runlots" title="Create 10,000 rows" cb={runLots} />
-              <Button id="add" title="Append 1,000 rows" cb={add} />
-              <Button id="update" title="Update every 10th row" cb={update} />
-              <Button id="clear" title="Clear" cb={clear} />
-              <Button id="swaprows" title="Swap Rows" cb={swapRows} />
-            </div>
-          </div>
+          <div className="col-md-6"><h1>react-tracked useSelector</h1></div>
+          <div className="col-md-6"><div className="row">
+            <Button id="run" title="Create 1,000 rows" cb={run} />
+            <Button id="runlots" title="Create 10,000 rows" cb={runLots} />
+            <Button id="add" title="Append 1,000 rows" cb={add} />
+            <Button id="update" title="Update every 10th row" cb={update} />
+            <Button id="clear" title="Clear" cb={clear} />
+            <Button id="swaprows" title="Swap Rows" cb={swapRows} />
+          </div></div>
         </div>
       </div>
       <table className="table table-hover table-striped test-data"><tbody><RowList /></tbody></table>
@@ -150,10 +120,6 @@ const Main = () => {
 };
 
 ReactDOM.render(
-  (
-    <Provider useValue={useValue}>
-      <Main />
-    </Provider>
-  ),
+  <Provider useValue={useValue}><Main /></Provider>,
   document.getElementById("main")
 );
